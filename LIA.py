9f94dc67fc665d09fd56753da6b5fc080db942e3
@fastner <fastner@cip.ifi.lmu.de>
2023-06-06 16:35:40 +0200

    update existing changes

--- LIA.py
+++ LIA.py
@@ -1,2 +1,34 @@
-#problem in screenshots
+#Given: Two positive integers k(k≤7) and N(N≤2k). 
+#In this problem, we begin with Tom, who in the 0th generation has genotype Aa Bb. 
+#Tom has two children in the 1st generation, each of whom has two children, and so on. 
+#Each organism always mates with an organism having genotype Aa Bb.
+
+#Return: The probability that at least N Aa Bb organisms will belong to the k-th generation of Tom's family tree 
+#(don't count the Aa Bb mates at each level). Assume that Mendel's second law holds for the factors.
+
+#Inputs
+k = 2
+N = 1
+out = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- REVP.py
+++ REVP.py
@@ -1,1 +1,64 @@
-#problem in screenshots
+#Given: A DNA string of length at most 1 kbp in FASTA format.
+#The position and length of every reverse palindrome in the string having length between 4 and 12. 
+#You may return these pairs in any order.
+
+#code to find reverse palindromes.
+
+#for each window size 4-12:
+    #starting at i, going through s:
+        #if i-i+window size is a palindrome:
+            #add i + " " + window size to out
+
+#for entry in out:
+    #print(entry)
+
+
+s = "GCGCCATCATTGCGACTGGTTACCACACCGCACTACCAAGAATTAACGCCCGTAGAAAGACAACTAGTAGGTGACTGCATACCCTCCGATCGAGCGACCGGTGGACACAATCGAACCTCCAGTCATTAAGAGAGGCAGCTCTGAACCCCGTGGCGCTAACACCTCTAAATAAACCGGTCCCATTGAGACTACGTGTCTTAATCCAACGCTGAAGAACATTGGACACCATGCTTGCGGCCCCGCCACGCATGTTCTCCGGGTCTTAGTCAATGAAGATCTGCACTTTAGAGACATGGCCCAAAATATCGCTTCGGCGGTTCTTCAGGCATCTCACAGAGCTCTCTGGTTTGCACCAAAGAGCCGAGGGAATACTTGGGTATGGAAACGATGCTAACGAAGTGTACACTTCCCTGCTACCCTTGGACTTTTGCCGCTGGGTCATCGCACTACACCCGCTGTCCGTCTCGATGCCTAGACTTGCTCTCCTTTGCCAGACAGTATCCCAAAAAGATCTCACCATTCACGGTCGTCCGACTATGTTGTCTATTACAGCGATGCACACAAGGGCCACCGCCAGGATACCAACAGCGGGTTGGTCGATTTCTAGTTTCGTATCCACAACAGCAAAAAAGACTCAAACGAAAATACGTAGGGGACCTTGCGGCCACGGAGTTGTGGCGAGGCGTAAAGAATGCACTGCCTTACTGTACGCATACGCGACCACCCATAAACTTAACGTTCCACTAATTTCGCAATAGATAGAGCATCCAATTGGGCAATAGGCGTATTTCCGTCAACTGCGCG"
+out = []
+
+def reverseComplement(sub):
+    new = ""
+    for letter in sub:
+        if letter == "A":
+            new += "T"
+        if letter == "C":
+            new += "G"
+        if letter == "G":
+            new += "C"
+        if letter == "T":
+            new += "A"
+    i = len(new) - 1
+    res = ""
+    while i >= 0:
+        res+=(new[i])
+        i -= 1
+    return res
+
+def isPalindrome(sub):
+    if reverseComplement(sub) == (sub):
+        return True
+    else:
+        return False
+
+w = 4
+while w < 12:
+    index = 0
+    while index < len(s) - w:
+        index += 1
+        sub = s[index:(index + w)]
+        if isPalindrome(sub):
+            b = [(index + 1), w]
+            out.append(b)
+    w += 1
+
+#print(out)
+out = sorted(out, key = lambda x : x[0], reverse = False)
+#print(out)
+
+#need to be tab not space
+#no newline at the end
+with open("REVPout.txt", "w") as o:
+    for entry in out:
+        for e in entry:
+            o.write(str(e) + "\t")
+        o.write("\n")
