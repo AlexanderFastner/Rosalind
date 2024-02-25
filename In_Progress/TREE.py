14ca397762574dfe4dfbaf62c7a822f5d71e0ac2
root <root@White>
2024-02-25 15:48:35 +0100

    solve PPER

---
+++ In_Progress/TREE.py
@@ -0,0 +1,16 @@
+#input: n= number of nodes, and l adjacency list of those nodes (edges)
+#output: min num of edges to make a tree
+
+#---------------------------------------------------------------------------------------------------
+
+
+#IDK the example doesnt seem right
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------
